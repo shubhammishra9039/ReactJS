39bc3d71f71755c5d29dd86e2cc4b6bf155e1662
Shubham Mishra <shubhammishra6595@gmail.com>
2024-09-10 07:21:43 +0530

    PasswordGenerator

---
+++ 05passwordGenretor/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1>hy</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 05passwordGenretor/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <App />
+);
